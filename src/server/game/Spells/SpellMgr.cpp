098e55fe2aea17584ec8b5ea0dc144c86e080baf
Gutu Mongoleca <go6ko1@abv.bg>
2011-10-03 23:30:28 +0300

    Core/Spells: Envenom buff duration

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -2947,6 +2947,12 @@ void SpellMgr::LoadDbcDataCorrections()
 			case 49611: // Magic Suppression Rank 3
 				spellInfo->procCharges = 0;
 				break;
+			case 32645: // Envenom (Rank 1)
+			case 32684: // Envenom (Rank 2)
+			case 57992: // Envenom (Rank 3)
+			case 57993: // Envenom (Rank 4)
+				spellInfo->Dispel = DISPEL_NONE;
+				break;
             case 49838: // Stop Time
                 spellInfo->AttributesEx3 |= SPELL_ATTR3_NO_INITIAL_AGGRO;
                 break;
